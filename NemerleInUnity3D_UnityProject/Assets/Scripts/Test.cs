4e68ab8176a27c83298db594429ef935dd1f5ba8
Rufat Lazhansky <rufat.lazhansky@dtigames.com>
2017-03-10 13:37:08 +0300

    UnityProject

---
+++ NemerleInUnity3D_UnityProject/Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+using UniRx;
+
+
+namespace NemerleInUnity
+{
+    public class Test : MonoBehaviour
+    {
+        private void Awake()
+        {
+        
+        }
+
+        private void Start()
+        {
+        
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
